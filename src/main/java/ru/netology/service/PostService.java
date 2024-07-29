878e84a70c8399d74f21d065485912e611b9dfb4
Sergey1995 <Serega19952@rambler.ru>
2024-07-29 23:00:42 +0300

    four commit

--- src/main/java/ru/netology/service/PostService.java
+++ src/main/java/ru/netology/service/PostService.java
@@ -6,7 +6,7 @@ import ru.netology.model.Post;
 import ru.netology.repository.IPostRepository;
 import java.util.List;
 
-
+@Service
 public class PostService {
     private final IPostRepository repository;
 
